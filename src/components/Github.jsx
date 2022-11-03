69d3ba7cbbb2d4587c60b15b4018c03281cfc426
Abrar2003 <spartanabrar@gmail.com>
2022-11-03 23:32:49 +0530

    added github stats

--- src/components/Github.jsx
+++ src/components/Github.jsx
@@ -4,10 +4,27 @@ import { color } from "@chakra-ui/react";
 import style from "./styles/github.module.css";
 
 export default function Github() {
-  
   return (
     <div className={style.main}>
-        <h1>My Contributions</h1>
+      <h1>Statistics</h1>
+      <div className={style.stats}>
+        <div>
+          <h1>1200+</h1>
+          <p>Hours spent on coding</p>
+        </div>
+        <div>
+          <h1>200+</h1>
+          <p>Solved DSA problems</p>
+        </div>
+        <div>
+          <h1>7</h1>
+          <p>Months of training on interpersonal skills</p>
+        </div>
+        <div>
+          <h1>6</h1>
+          <p>Projects completed</p>
+        </div>
+      </div>
       <div className={style.calendar}>
         <GitHubCalendar
           username="abrar2003"
@@ -16,6 +33,10 @@ export default function Github() {
           blockSize={15}
           blockMargin={5}
         />
+      </div>
+      <div className={style.gitStats}>
+        <img src="https://github-readme-stats.vercel.app/api?username=Abrar2003&theme=gotham&hide_border=false&include_all_commits=true&count_private=true&show_icons=true" />
+        <img src="https://github-readme-streak-stats.herokuapp.com/?user=Abrar2003&theme=gotham&hide_border=false" />
       </div>
     </div>
   );
